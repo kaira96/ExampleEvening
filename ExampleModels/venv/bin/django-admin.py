9567f703cfcca8b5021ddd4687658dbadd69df28
tenthent <kayratsagynbekov@gmail.com>
2021-03-01 21:20:08 +0600

    Удалил виртуальное окружение добавил файл link.txt

--- ExampleModels/venv/bin/django-admin.py
+++ ExampleModels/venv/bin/django-admin.py
@@ -1,4 +1,4 @@
-#!/home/tenthent/PycharmProjects/ExampleModels/venv/bin/python
+#!/home/tenthent/ExampleEvening/ExampleModels/venv/bin/python
 # When the django-admin.py deprecation ends, remove this script.
 import warnings
 
