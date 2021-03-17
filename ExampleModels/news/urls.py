012b7fa6ae5fd72b94fbb0e9c214fad1f0b0e11d
tenthent <kayratsagynbekov@gmail.com>
2021-03-17 18:44:17 +0600

    remove ExampleMpdels

--- ExampleModels/news/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-
-from news.views import *
-
-urlpatterns = [
-    path('', index, name='home'),
-    path('category/<int:category_id>', get_category, name='category')
-]
